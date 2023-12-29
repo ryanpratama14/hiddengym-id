d36acb9c37eb7484169308381213673f9e55e4cb
Ryan Pratama <ru.ryanpratama@gmail.com>
2023-12-30 00:04:16 +0700

    adjust

--- src/app/[lang]/dashboard/owner/sport-types/create/page.tsx
+++ src/app/[lang]/dashboard/owner/sport-types/create/page.tsx
@@ -1,8 +1,5 @@
 import { type Locale } from "@/i18n.config";
 import { useDictionary } from "@/lib/dictionary";
-import { type SportCreateInput } from "@/server/api/routers/sport";
-import { api } from "@/trpc/server";
-import { revalidatePath } from "next/cache";
 import CreateSportForm from "./components/CreateSportForm";
 
 type Props = {
@@ -12,17 +9,10 @@ type Props = {
 export default async function SportTypeCreatePage({ params }: Props) {
   const t = await useDictionary(params.lang);
 
-  const createData = async (data: SportCreateInput) => {
-    "use server";
-    const res = await api.sport.create.mutate(data);
-    revalidatePath("/");
-    return res;
-  };
-
   return (
     <section className="main-create-padding">
       <h3>Create Sport Type</h3>
-      <CreateSportForm t={t} createData={createData} lang={params.lang} />
+      <CreateSportForm t={t} lang={params.lang} />
     </section>
   );
 }
