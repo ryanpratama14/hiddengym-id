c88a6caf270b38db15a7cb77f0c4d2960934f911
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-03 17:48:52 +0700

    adjust

---
+++ src/app/[lang]/secret/page.tsx
@@ -0,0 +1,13 @@
+export default function page() {
+  // await api.user.createVisitor.mutate({
+  //   visitorData: {
+  //     email: "modric@realmadrid.sp",
+  //     fullName: "Luka Modric",
+  //     phoneNumber: "81210425919",
+  //     birthDate: "2000/05/01",
+  //     gender: "MALE",
+  //   },
+  // });
+
+  return <div>page</div>;
+}
