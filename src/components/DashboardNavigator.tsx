241d1850a8c5a046585548f2abc3b44bb92a4cfd
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-05 09:40:33 +0700

    adjust

--- src/components/DashboardNavigator.tsx
+++ src/components/DashboardNavigator.tsx
@@ -1,18 +1,36 @@
+import { USER_REDIRECT } from "@/lib/constants";
 import { cn } from "@/lib/functions";
+import { type Lang } from "@/types";
+import { type Role } from "@prisma/client";
+import Link from "next/link";
 
 const DashboardNavigator = ({
+  href,
+  newTab,
   className,
+  role,
   isChildren,
   children,
+  lang,
 }: {
+  href: string;
   isChildren?: boolean;
   children: React.ReactNode;
   className?: string;
+  newTab?: boolean;
+  target?: React.HTMLAttributeAnchorTarget;
+  role: Role;
+  lang: Lang;
 }) => {
   return (
-    <button type="button" className={cn(`text-base select-none font-medium ${className}`, { "ml-3 text-sm": isChildren })}>
+    <Link
+      target={newTab ? "_blank" : undefined}
+      rel={newTab ? "noopener noreferrer" : undefined}
+      className={cn(`text-base select-none font-medium ${className}`, { "ml-3 text-sm": isChildren })}
+      href={USER_REDIRECT[role]({ lang, href })}
+    >
       {children}
-    </button>
+    </Link>
   );
 };
 
