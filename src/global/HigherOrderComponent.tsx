ef6da386d78cb374a730dd27559558c73cc03fb8
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-21 00:47:07 +0700

    adjust

--- src/global/HigherOrderComponent.tsx
+++ src/global/HigherOrderComponent.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import Logo from "@/components/Logo";
 import { useZustand } from "@/global/store";
+import { COLORS } from "@/styles/theme";
 import { type Lang } from "@/types";
 import { type Session } from "next-auth";
 import { signOut } from "next-auth/react";
 import { Suspense, useEffect } from "react";
+import { ClipLoader } from "react-spinners";
 
 type Props = {
   lang: Lang;
@@ -23,5 +26,20 @@ export default function HigherOrderComponent({ lang, session, children, isSessio
     if (lang) setLang(lang);
   }, [session, lang]);
 
-  return <Suspense>{children}</Suspense>;
+  return (
+    <Suspense
+      fallback={
+        <main className="h-[100dvh] items-center justify-center flex flex-col gap-6 p-shorter">
+          <Logo className="w-72 aspect-video" />
+          <section className="flex flex-col text-center">
+            <h6>HIDDEN GYM</h6>
+            <small className="italic">eat sleep gym repeat</small>
+          </section>
+          <ClipLoader color={COLORS.orange} size={30} />
+        </main>
+      }
+    >
+      {children}
+    </Suspense>
+  );
 }
