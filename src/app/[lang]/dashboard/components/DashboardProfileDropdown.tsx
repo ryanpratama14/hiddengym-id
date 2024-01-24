2a94b03df40ff4b6e433c90dab7a63feda669c41
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-01-24 09:17:19 +0700

    adjust

--- src/app/[lang]/dashboard/components/DashboardProfileDropdown.tsx
+++ src/app/[lang]/dashboard/components/DashboardProfileDropdown.tsx
@@ -4,13 +4,10 @@ import ModalConfirm from "@/components/ModalConfirm";
 import { GENDERS, ICONS, PROFILE_BUTTON_ITEMS_TO_REMOVE } from "@/lib/constants";
 import { createUrl } from "@/lib/functions";
 import { type User } from "@/server/api/routers/user";
-import { COLORS } from "@/styles/theme";
 import type { NewParamsAction, ProfileButtonKey } from "@/types";
 import { Menu, Transition } from "@headlessui/react";
 import { type IconifyIcon } from "@iconify/react/dist/iconify.js";
 import type { Role } from "@prisma/client";
-import { Menu as AntdMenu, ConfigProvider } from "antd";
-import type { ItemType, MenuItemType } from "antd/es/menu/hooks/useItems";
 import { signOut } from "next-auth/react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Fragment } from "react";
@@ -47,17 +44,6 @@ export default function DashboardProfileDropdown({ user }: Props) {
   const FILTERED_PROFILE_BUTTON_ITEMS = (role: Role) =>
     PROFILE_BUTTON_ITEMS.filter((btn) => !PROFILE_BUTTON_ITEMS_TO_REMOVE[role].includes(btn.key));
 
-  const items: ItemType<MenuItemType>[] = FILTERED_PROFILE_BUTTON_ITEMS(user.role).map((e, index) => ({
-    title: "",
-    key: index,
-    icon: <Iconify icon={e.icon} width={20} />,
-    label: (
-      <button onClick={e.onClick} type="button" className="font-medium text-sm w-full h-full text-left">
-        {e.label}
-      </button>
-    ),
-  }));
-
   return (
     <Fragment>
       <ModalChangePassword show={!!searchParams.get("changePassword")} closeModal={() => redirect("changePassword", "delete")} />
@@ -67,38 +53,42 @@ export default function DashboardProfileDropdown({ user }: Props) {
         closeModal={() => redirect("signOut", "delete")}
         show={!!searchParams.get("signOut")}
       />
-      <ConfigProvider theme={{ components: { Menu: { itemHeight: 30, itemHoverBg: COLORS.orange, itemBorderRadius: 6 } } }}>
-        <Menu as="article" className="relative">
-          <Menu.Button className="size-10 bg-cream rounded-full relative shadow border-2 border-cream">
-            <section>
-              {user?.image?.url ? (
-                <Img
-                  src={user.image.url}
-                  alt="Profile Picture"
-                  className="absolute centered object-cover w-full h-full rounded-full"
-                />
-              ) : (
-                <Iconify icon={GENDERS[user.gender].picture} className="absolute centered text-dark" width={35} />
-              )}
-            </section>
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition duration-300"
-            enterFrom="transform -translate-y-2 opacity-0"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition duration-300"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform -translate-y-2 opacity-0"
-          >
-            <Menu.Items className="active:outline-none focus:outline-none outline-none absolute right-0 mt-4 rounded-md origin-top-right bg-dark shadow-lg">
-              <Menu.Item>
-                <AntdMenu inlineIndent={14} color={COLORS.dark} mode="inline" items={items} style={{ borderRadius: 6 }} />
+
+      <Menu as="article" className="relative">
+        <Menu.Button className="size-10 bg-cream rounded-full relative shadow border-2 border-cream">
+          <section>
+            {user?.image?.url ? (
+              <Img src={user.image.url} alt="Profile Picture" className="absolute centered object-cover w-full h-full rounded-full" />
+            ) : (
+              <Iconify icon={GENDERS[user.gender].picture} className="absolute centered text-dark" width={35} />
+            )}
+          </section>
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition duration-300"
+          enterFrom="transform -translate-y-2 opacity-0"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition duration-300"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform -translate-y-2 opacity-0"
+        >
+          <Menu.Items className="flex flex-col w-48 gap-1 active:outline-none focus:outline-none outline-none absolute p-1 right-0 mt-4 rounded-md origin-top-right bg-dark shadow-lg">
+            {FILTERED_PROFILE_BUTTON_ITEMS(user.role).map((e) => (
+              <Menu.Item key={e.key}>
+                <button
+                  type="button"
+                  onClick={e.onClick}
+                  className="flex font-medium gap-2 items-center px-2 bg-dark text-cream hover:bg-orange rounded-md text-sm py-1"
+                >
+                  <Iconify icon={e.icon} width={20} />
+                  {e.label}
+                </button>
               </Menu.Item>
-            </Menu.Items>
-          </Transition>
-        </Menu>
-      </ConfigProvider>
+            ))}
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </Fragment>
   );
 }
