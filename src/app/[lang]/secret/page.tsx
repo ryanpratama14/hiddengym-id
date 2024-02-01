323791611895f7b85b747b14d691bf079ad8a799
Ryan Pratama <ru.ryanpratama@gmail.com>
2024-02-01 13:29:35 +0700

    adjust

--- src/app/[lang]/secret/page.tsx
+++ src/app/[lang]/secret/page.tsx
@@ -1,14 +1,3 @@
-import { api } from "@/trpc/server";
-
 export default async function page() {
-  await api.user.create.mutate({
-    email: "owner@hiddengym-id.com",
-    fullName: "Ryan Pratama",
-    credential: "hiddengym-id@2023",
-    phoneNumber: "81210425333",
-    birthDate: "2000-07-14",
-    gender: "MALE",
-  });
-
   return <div>page</div>;
 }
